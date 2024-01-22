9ac3b9f1590412140aaeece4bedd09e5778662b1
DESKTOP-FS9S6MU\olgad <olgad20002@yandex.ru>
2024-01-22 14:33:55 +0300

    Created project

---
+++ Assets/Scripts/lab.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuManager : MonoBehaviour
+
+{
+    public GameObject settingsPanel;
+
+    public void PlayGame()
+    {
+        Application.LoadLevel("G");
+    }
+
+    public void ExitGame()
+    {
+        Application.Quit();
+    }
+
+    public void Se3ttingsPanel()
+    {
+        settingsPanel.SetActive(true);
+    }
+
+    public void Exit()
+    {
+        settingsPanel.SetActive(false);
+    }
+}
